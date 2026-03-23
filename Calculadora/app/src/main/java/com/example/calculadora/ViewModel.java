66c0441cdc03a024e5757477e6eee69c185e0e6f
Mario Bros <director@fca.unam.mx>
2026-03-23 08:08:20 -0600

    Clase 5

--- Calculadora/app/src/main/java/com/example/calculadora/ViewModel.java
+++ Calculadora/app/src/main/java/com/example/calculadora/ViewModel.java
@@ -1,25 +1,29 @@
 package com.example.calculadora;
 
 public class ViewModel {
-    ICalculadora calculadora = new Calculadora();
-
+    private ICalculadora calculadora = new Calculadora();
     ViewModel() {
 
     }
-
-    Double sum(double x, double y) {
-        return calculadora.sum(x, y);
+    Double makeOperation(Operacion [] operations) {
+        Double cache = 0.0;
+        for (Operacion operation: operations) {
+            cache += makeOperation(operation);
+        }
+        return cache;
     }
-
-    Double minus(double x, double y) {
-        return calculadora.minus(x, y);
-    }
-
-    Double multiply(double x, double y) {
-        return calculadora.multiply(x, y);
-    }
-
-    Double divide(double x, double y) {
-        return calculadora.divide(x, y);
+    Double makeOperation(Operacion operacion) {
+        switch (operacion.getType()) {
+            case ADD:
+                return calculadora.sum(operacion.x, operacion.y);
+            case MINUS:
+                return calculadora.minus(operacion.x, operacion.y);
+            case MULTIPLY:
+                return calculadora.multiply(operacion.x, operacion.y);
+            case DIV:
+                return calculadora.divide(operacion.x, operacion.y);
+            default:
+                return 0.0;
+        }
     }
 }
